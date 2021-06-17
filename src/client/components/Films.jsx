8f5580f77e0cb74397c062e39e081333c1a7aaa2
Dmytro Iievliev <dmytro.iievliev@adidas.com>
2021-06-17 09:38:07 +0200

    added film links

--- src/client/components/Films.jsx
+++ src/client/components/Films.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default ({ films }) => (
   <>
     <h1>Studio Ghibli Films</h1>
     <ul>
       {films.map((film, idx) => (
-        <li key={idx}>{film.title}</li>
+        <li key={idx}>
+          <Link to={`/films/${film.id}`}>{film.title}</Link>
+        </li>
       ))}
     </ul>
   </>
