ffd1f87767ac52dd852f5d8d960f3eab7c27b65b
Dmytro Iievliev <dmytro.iievliev@adidas.com>
2021-06-24 00:01:18 +0200

    refactoring

--- src/client/components/Films.jsx
+++ src/client/components/Films.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default ({ films }) => {
-  const filmIds = Object.keys(films);
+  const ids = Object.keys(films);
 
   return (
     <>
       <h1>Studio Ghibli Films</h1>
       <ul>
-        {filmIds.map((id) => (
+        {ids.map((id) => (
           <li key={id}>
             <Link to={`/films/${id}`}>{films[id].title}</Link>
           </li>
--- src/client/store/reducers/index.js
+++ src/client/store/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from "redux";
 
-import filmsReducer from "./filmsReducer";
-import filmDetailsReducer from "./filmDetails";
-import charactersReducer from "./charactersReducer";
-import characterDetailsReducer from "./characterDetails";
+import films from "./films";
+import filmDetails from "./filmDetails";
+import characters from "./characters";
+import characterDetails from "./characterDetails";
 
 export default combineReducers({
-  films: filmsReducer,
-  filmDetails: filmDetailsReducer,
-  characters: charactersReducer,
-  characterDetails: characterDetailsReducer,
+  films,
+  filmDetails,
+  characters,
+  characterDetails,
 });
